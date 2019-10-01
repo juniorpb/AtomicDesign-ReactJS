5aa4b2d52ad9efaf1d7d787774f93db7385b66c0
juniorpb <juniorfoxld@hotmail.com>
2019-10-01 13:50:20 -0300

    tela login finalizado

--- src/containers/pages/Nubank/login.jsx
+++ src/containers/pages/Nubank/login.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Header, Container } from '../../../components'
+import logo from '../../../assets/images/logo_branco.svg';
+import { Header, Container, Link } from '../../../components';
+import loginStyle from './login-style.css';
+
 export default function NubankLogin() {
     return (
-        <div>
-
-            <Header />
-            <Container>LOGIN</Container>
+        <div className="img-branco">
+            <a href="/nubank/">
+                <img  src={logo} width="80" />
+            </a>
+            {/* <Header /> */}
+            <div className="login-container">
+                <form>
+                    <h3>Faça seu login</h3>
+                    <input placeholder="CPF" name="" />
+                    <input placeholder="Senha" name="" />
+                    <button type="submit">CONTINUAR</button>
+                </form>
+            </div>
         </div>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, BrowserRouter} from 'react-router-dom';
 
 import { Nubank } from './containers/pages';
+import { LoginNubank } from './containers/pages';
 
 export default function Routes() {
     return (
         <BrowserRouter>
-            <Route path='/nubank' component={Nubank} />
+            <Route path='/nubank' exact component={Nubank} />
+            <Route path='/nubank/login' component={LoginNubank} />
         </BrowserRouter>
     )
 }
