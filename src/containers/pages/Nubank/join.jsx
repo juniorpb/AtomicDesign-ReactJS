7db75e62f23d2356bf1866ba2a2d95b3b88c0fb0
juniorpb <juniorfoxld@hotmail.com>
2019-10-02 17:47:05 -0300

    tela join

---
+++ src/containers/pages/Nubank/join.jsx
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+import {useDispatch} from 'react-redux';
+
+import logo from '../../../assets/images/logo_branco.svg';
+import loginStyle from './login-style.css';
+import { Button, Header } from '../../../components';
+
+
+export default function NubankJoin() {
+
+    const [cpf, setCpf] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const dispatch = useDispatch();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    function enter () {
+        dispatch({type: 'ADD_DADOS', name: cpf})
+    }
+
+    return (
+        <div className="img-branco">
+            <a href="/nubank/">
+                <img  src={logo} width="80" />
+            </a>
+
+            <Header />
+            <div className="login-container">
+                <form onSubmit={handleSubmit}>
+                    <h3>Juntese ao Nubank</h3>
+                    <input 
+                        placeholder="CPF"
+                        value={cpf} 
+                        onChange={(e)=> {
+                            console.log(cpf, e.target.value);
+                            setCpf(e.target.value);
+                        }}
+                    />
+
+                    <input 
+                        placeholder="Senha"
+                        value={senha}
+                        onChange={(e)=>{
+                            setSenha(e.target.value)
+                        }}
+                    />
+
+                    <Button 
+                        disabled={cpf.length>2 ? 0: 1}
+                        bgcolor={ cpf.length>2 ? '#fff':''}
+                        colorH={cpf.length>2 ? '#81259D':'#c3c3c3'}
+                        onClick={enter}
+                    >CONTINUAR</Button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+NubankJoin.propTypes = {
+    history: PropTypes.object.isRequired
+};
